7907f93a97d8d53d4501cd0efe47c479a2ad52af
Haotian Yang <haotian.ut@gmail.com>
2020-11-08 22:59:38 -0500

    Newton kind work

--- include/implicit_euler.h
+++ include/implicit_euler.h
@@ -20,6 +20,33 @@ template<typename ENERGY, typename FORCE, typename STIFFNESS>
 inline void implicit_euler(Eigen::VectorXd &q, Eigen::VectorXd &qdot, double dt, 
                             const Eigen::SparseMatrixd &mass,  ENERGY &energy, FORCE &force, STIFFNESS &stiffness, 
                             Eigen::VectorXd &tmp_qdot, Eigen::VectorXd &tmp_force, Eigen::SparseMatrixd &tmp_stiffness) {
-    
+
+
+    auto Jacobian = [&](Eigen::VectorXd &tmp_g, Eigen::VectorXd x0)
+    {
+        tmp_force.setZero();
+        force(tmp_force, q+(dt*x0), x0);
+        tmp_g = mass*(x0 - qdot) + dt * (-tmp_force);
+        //std::cout << "tmp_g\n" << tmp_g <<  std::endl;
+    };
+
+    auto Hessian = [&](Eigen::SparseMatrixd &tmp_H, Eigen::VectorXd x0)
+    {
+        tmp_stiffness.setZero();
+        stiffness(tmp_stiffness, q+(dt*x0), x0);
+        tmp_H = mass + dt * dt * (-tmp_stiffness);
+        //std::cout << "tmp_H\n" << tmp_H <<  std::endl;
+    };
+
+    tmp_qdot = qdot;
+    Eigen::VectorXd tmp_g;
+    Eigen::SparseMatrixd tmp_H;
+    tmp_g.setZero();
+    tmp_H.setZero();
+    newtons_method(tmp_qdot, energy, Jacobian, Hessian, 5, tmp_g, tmp_H);
+    qdot = tmp_qdot;
+    //std::cout << "qdot:" << qdot <<  std::endl;
+
+    q = q + dt*qdot;
 
 }
--- include/newtons_method.h
+++ include/newtons_method.h
@@ -12,6 +12,42 @@
 //  x0 - update x0 to new value
 template<typename Objective, typename Jacobian, typename Hessian>
 double newtons_method(Eigen::VectorXd &x0, Objective &f, Jacobian &g, Hessian &H, unsigned int maxSteps, Eigen::VectorXd &tmp_g, Eigen::SparseMatrixd &tmp_H) {
-   
+
+    double p = 0.5; // scaling factor
+    double alpha = 1.0; // initial step length
+    double c = 1e-8; // ensure sufficient decrease
+    //solve the linear system H*d = -g to the d
+    // A = H
+    // b = -g
+    for(int i = 0; i < maxSteps; i++)
+    {
+        g(tmp_g, x0);
+        H(tmp_H, x0);
+        if(tmp_g.squaredNorm() < c)
+        {
+            break;
+        }
+        Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>> solver;
+        solver.compute(tmp_H);
+        Eigen::VectorXd d;
+        d = solver.solve(-tmp_g);
+        //std::cout << "d\n" << d <<  std::endl;
+        //std::cout << "x0\n" << x0 <<  std::endl;
+
+        // line search
+        while(true)
+        {
+           double threshold_cost = f(x0) + c*d.transpose()*tmp_g;
+           double new_cost = f(x0 + (alpha*d));
+           if(new_cost <= threshold_cost || alpha < c)
+           {
+               std::cout << "alpha\n" << alpha <<  std::endl;
+               break;
+           }
+           alpha = p*alpha;
+        }
+        x0 = x0 + alpha*d;
+    }
+
    return 0.0;
 }
